dd0c8b259fbdcf463c240fda065f196abf886e25
Your Name <lucascampanharo19@gmail.com>
2025-07-01 14:53:31 -0300

    mongodb

--- node/exercícios/server.js
+++ node/exercícios/server.js
@@ -1,5 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.CONNECTIONSTRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.emit("pronto");
+  })
+  .catch((e) => console.log(e));
+
 const routes = require("./routes");
 const path = require("path");
 const { middlewareGlobal } = require("./src/middlewares/middleware");
@@ -15,7 +29,9 @@ app.set("view engine", "ejs");
 app.use(middlewareGlobal);
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log("Acessar http://localhost:3000");
-  console.log("Servidor executando na porta 3000");
+app.on("pronto", () => {
+  app.listen(3000, () => {
+    console.log("Acessar http://localhost:3000");
+    console.log("Servidor executando na porta 3000");
+  });
 });
